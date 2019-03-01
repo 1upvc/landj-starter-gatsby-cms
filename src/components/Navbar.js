62e0700ed094ff1fcf7b27fea404dbe4497c6769
junie87 <arjun@1upvc.com>
2019-02-28 22:46:34 -0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
-              <Link className="navbar-item" to="/contact">
+              <Link className="navbar-item" to="/shop">
                 Shop
               </Link>
               <Link className="navbar-item" to="/contact/examples">
