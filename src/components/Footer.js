c688beb3b72154437967b3d230adc3324cecea9a
junie87 <arjun@1upvc.com>
2019-02-28 23:19:30 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -53,8 +53,8 @@ const Footer = class extends React.Component {
                     </Link>
                   </li>
                   <li>
-                    <Link className="navbar-item" to="/contact">
-                      Contact
+                    <Link className="navbar-item" to="/Shop">
+                      Shop
                     </Link>
                   </li>
                   </ul>
