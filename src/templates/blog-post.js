b7ece1bae1ea094ef4ad918daff8d38ec9370374
junie87 <arjun@1upvc.com>
2019-03-01 08:55:16 -0500

    Update blog-post.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -93,7 +93,7 @@ export const pageQuery = graphql`
       id
       html
       frontmatter {
-        date
+        date(formatString: "MMMM DD, YYYY")
         title
         description
         tags
