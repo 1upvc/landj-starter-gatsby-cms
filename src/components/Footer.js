bfa4af1ad2f6bcb6ead47dea98f3e0e1284b6bfa
junie87 <arjun@1upvc.com>
2019-02-28 21:55:03 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = class extends React.Component {
         <div className="content has-text-centered">
           <img
             src={logo}
-            alt="Kaldi"
+            alt="L&J Family Foods"
             style={{ width: '14em', height: '10em' }}
           />
         </div>
@@ -63,9 +63,9 @@ const Footer = class extends React.Component {
                 </section>
                 </div>
                 <div className="column is-4 social">
-                
+
                   <a title="facebook" href="https://facebook.com">
-                    <img 
+                    <img
                       src={facebook}
                       alt="Facebook"
                       style={{ width: '1em', height: '1em' }}
