0282c3605220a02abecff5117450b666f6a18dd7
junie87 <arjun@1upvc.com>
2019-03-02 09:20:24 -0500

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -87,7 +87,7 @@ export const IndexPageTemplate = ({
               <div className="columns">
               <div className="column is-12 has-text-centered">
                 <Link className="btn" to="/products">
-                  Explore Products
+                  Explore Product
                 </Link>
                 </div>
               </div>
