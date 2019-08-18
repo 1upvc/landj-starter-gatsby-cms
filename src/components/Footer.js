81f8a9de1815b2af720619f082dc5c238b829d0a
junie87 <arjun@1upvc.com>
2019-08-18 16:10:49 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 import logo from '../img/logo2.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
-import twitter from '../img/social/twitter.svg'
+import vimeo from '../img/social/vimeo.svg'
 import linkedin from '../img/social/linkedin.svg'
 
 const Footer = class extends React.Component {
