95a4c17b2306d69809a9f5e5eb29d258dbf9efc2
junie87 <arjun@1upvc.com>
2019-11-07 07:22:58 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import logo from '../img/logo2.svg'
+import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import vimeo from '../img/social/vimeo.svg'
 import linkedin from '../img/social/linkedin.svg'
@@ -68,7 +69,12 @@ const Footer = class extends React.Component {
                 </div>
                 <div className="column is-4 social">
 
-
+                  <a title="facebook" href="https://facebook.com">
+                    <img
+                      src={facebook}
+                      alt="Facebook"
+                      style={{ width: '1em', height: '1em' }}
+                    />
                   </a>
                   <a title="twitter" href="https://vimeo.com/landjfamilyfoods">
                   <img
