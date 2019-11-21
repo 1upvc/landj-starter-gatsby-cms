dad342d1fca15341253e97bca42ac582b36fe58f
junie87 <arjun@1upvc.com>
2019-11-21 18:15:18 -0500

    Update index.js

--- src/pages/media/index.js
+++ src/pages/media/index.js
@@ -26,7 +26,7 @@ export default class BlogIndexPage extends React.Component {
                     padding: '1rem',
                   }}
                 >
-                  Quarterly Reads
+                  Related Reads
                 </h1>
               </div>
             </div>
